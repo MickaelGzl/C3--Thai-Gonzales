04bd74a61f7a6af1a38d8dff8a96e30b74bad10b
Mickael Gonzales <mickael.gonzales@hotmail.com>
2024-03-02 17:26:34 +0100

    fatal error: JavaScript heap out of memory if playersPerTeams is not a number

--- test/teamGenerator.test.js
+++ test/teamGenerator.test.js
@@ -31,8 +31,17 @@ describe("Testing the Team Generation Functions", function () {
     done();
   });
 
-  it("2. should not create teams if players param is a string ", function (done) {
+  it("3. should not create teams if players param is a string ", function (done) {
     const teamGenerator = new TeamGenerator("Jean luc Melenchon");
+    teamGenerator.generateTeams();
+
+    expect(teamGenerator.getTeams()).to.have.lengthOf(0);
+
+    done();
+  });
+
+  it("4. should send an error if playersPerTeams is not a number", function (done) {
+    const teamGenerator = new TeamGenerator(DEFAULT_PLAYERS, "toto");
     teamGenerator.generateTeams();
 
     expect(teamGenerator.getTeams()).to.have.lengthOf(0);
